e6cf58e47b3de525e0e658e002a4433ccf2c2e28
«SERGEY» <szely@yandex>
2022-09-18 19:00:04 +0300

    Parameters entered

--- PJ/Program.cs
+++ PJ/Program.cs
@@ -1,2 +1,12 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача: Написать программу, которая из имеющегося массива строк формирует массив из строк, длина которых меньше либо равна 3 символа. 
+// Первоначальный массив можно ввести с клавиатуры либо задать на старте выполнения алгоритма. 
+// При решении не рекомендуется пользоваться коллекциями, лучше обойтись исключитмассивами.
+// Примеры: 
+// [ "hello", "2", "world", ":-)" ];
+// [ "1234", "1567", "-2", "computer science" ]
+// [ "Russia", "Denmark", "Kazan"]
+using static System.Console;
+Clear();
+string[] stringA = new string[] { "hello", "2", "world", ":-)" };
+string[] stringB = new string[] { "1234", "1567", "-2", "computer science" };
+string[] stringC = new string[] { "Russia", "Denmark", "Kazan"};
